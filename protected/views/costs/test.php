f0e0ee151d309b5d013f0c901319abaf84def05a
asfaquz <asfaquz@gmail.com>
2014-11-09 20:06:51 +0530

    htaccess added to hide index.php in url

--- protected/views/costs/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * echo $testand open the template in the editor.
- */
-
-echo $test;
-?>
---
+++ themes/custom_theme/views/layouts/_footer.php
@@ -0,0 +1,22 @@
+<footer id="footer">
+    <!-- You're free to remove the credit link to Jayj.dk in the footer, but please, please leave it there :) -->
+    <p>
+        Copyright &copy; 2011 <a href="#">souq.com</a>
+        <span class="sep">|</span>
+
+    </p>
+</footer> <!-- #footer -->
+
+<div class="clear"></div>
+
+<!--[if IE]><?php //Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/js/excanvas.js'); ?><![endif]-->   
+<?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/js/script.js'); ?>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+
+
+
+</body>
+</html>
